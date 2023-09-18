7b70eba8bcff01c3da6745741f91bf73a246d006
Yuvraj Potulgude <137260804+YuvrajPotulgude@users.noreply.github.com>
2023-09-18 12:49:28 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,37 @@
+# python basic operations
+
+print("hello world")
+n=4
+for i in range(0,n):
+    print(i)
+    
+  
+a=7
+b=5
+if(a>b):
+    print("a is greater than b")
+    
+else:
+  print("b is greater than b")  
+  
+print(a)
+print(type(a))
+converted_a=str(a)
+print(type(converted_a))
+s="yuvraj"
+print(len(s))
+x=s.capitalize()
+print(x)
+y=s.casefold()
+print(y)
+def sum(a,b):
+   print(a+b)
+   
+sum(6,7)
+  
+
+
+
+
+
+
